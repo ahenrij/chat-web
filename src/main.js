0b18e16e04cd35a19bd0c8e0f8fdac84ecc3cb07
Henri Aïdasso <ahenrij@gmail.com>
2022-05-14 00:24:26 +0200

    style css added

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/css/main.css";
+import "./assets/css/style.css";
 
 createApp(App).use(store).use(router).mount("#app");
