1d3ab75f36a165da47d1916f6be15d8e7c60ed62
Henri Aïdasso <ahenrij@gmail.com>
2022-05-16 15:06:53 +0200

    join room router added

---
+++ src/views/JoinRoomView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <JoinRoom />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import JoinRoom from "@/components/JoinRoom.vue";
+
+export default {
+  name: "JoinRoomView",
+  components: {
+    JoinRoom,
+  },
+};
+</script>
