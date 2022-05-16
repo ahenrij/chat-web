d2c0bf99a86ad7ac8c411be027c191df1bab4689
Henri Aïdasso <ahenrij@gmail.com>
2022-05-16 15:09:27 +0200

    btn text updated

--- src/components/room/JoinRoom.vue
+++ src/components/room/JoinRoom.vue
@@ -26,7 +26,7 @@
           type="button"
           class="bg-blue-500 hover:bg-blue-600 text-white w-full rounded-md p-3 md:p-4"
         >
-          Join
+          Join now
         </button>
       </div>
       <p class="text-center">or</p>
