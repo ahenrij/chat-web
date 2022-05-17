b290dbcfeb7fb6cd78a17e128a9032e581c9194d
Henri Aïdasso <ahenrij@gmail.com>
2022-05-18 01:24:18 +0200

    home view updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="home">
-    <h2>Welcome to Web Chat</h2>
+  <div
+    style="margin-top: -100px"
+    class="grid grid-cols-2 items-center h-screen mx-10"
+  >
+    <div class="p-10">
+      <h1 class="md:text-8xl text-4xl">Web Chat</h1>
+      <p class="text-2xl pr-10 mt-5">
+        Create or join a chat room for real-time discussions with friends and
+        family about any interesting topic.
+      </p>
+      <router-link class="btn-primary" to="/create">Start a room</router-link>
+    </div>
+    <div>
+      <img src="@/assets/img/undraw_quick_chat.svg" alt="" />
+    </div>
   </div>
 </template>
 
