c19a84e2fd3ec0ff699735e231985d18ec566143
Henri Aïdasso <ahenrij@gmail.com>
2022-05-18 01:40:23 +0200

    message bubble added

---
+++ src/components/room/MessageBubble.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="message-bubble" :class="me">
+    <span class="from" :class="me"></span>
+    <br :class="me" />
+    <span class="message-text"></span>
+  </div>
+</template>
+<script>
+export default {
+  name: "message-bubble",
+
+  data() {
+    return {};
+  },
+
+  props: ["uuid", "text"],
+
+  computed: {
+    me() {
+      let result = false;
+
+      // Check if the client uuid of the message received is your client uuid
+      if (this.$store.getters.get("me").uuid === this.uuid) {
+        result = true;
+      }
+
+      // Render the message bubble on the right side if it is from this client
+      return result ? "me" : "";
+    },
+  },
+};
+</script>
