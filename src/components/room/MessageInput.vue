c7010350fb39532b8d0062ed98e1acb87929a815
Henri Aïdasso <ahenrij@gmail.com>
2022-05-18 01:36:00 +0200

    message input added

--- src/components/room/MessageInput.vue
+++ src/components/room/MessageInput.vue
@@ -1,3 +1,32 @@
 <template>
-  <div></div>
+  <div class="message-input">
+    <textarea
+      class="w-full"
+      ref="messageInput"
+      placeholder="message..."
+      maxlength="20000"
+      @keydown.enter="sendMessage"
+    ></textarea>
+  </div>
 </template>
+<script>
+export default {
+  methods: {
+    submitMessage(event) {
+      if (!event.shiftKey) {
+        event.preventDefault();
+      } else {
+        return;
+      }
+      // If the message body is empty, do not submit
+      if (event.target.value.length === 0) {
+        return;
+      }
+
+      // TODO: Make send message request
+
+      event.target.value = "";
+    },
+  },
+};
+</script>
