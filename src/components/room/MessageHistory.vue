0decc664776abcac52cc527a5be7605478dd8227
Henri Aïdasso <ahenrij@gmail.com>
2022-05-18 10:49:52 +0200

    history comp added

--- src/components/room/MessageHistory.vue
+++ src/components/room/MessageHistory.vue
@@ -1,3 +1,41 @@
 <template>
-  <div></div>
+  <div class="chat-history">
+    <message-bubble
+      v-for="msg in history"
+      v-bind:key="msg.id"
+      v-bind:uuid="msg.sender"
+      v-bind:text="msg.message"
+    >
+    </message-bubble>
+  </div>
 </template>
+<script>
+import MessageBubble from "@components/room/MessageBubble.vue";
+// import { mapGetters } from "vuex";
+
+function scrollBottom() {
+  this.$el.scrollTo(0, this.$el.scrollHeight);
+}
+
+export default {
+  data() {
+    return {
+      history: [],
+    };
+  },
+
+  mounted() {
+    this.history = []; //make request to get history
+  },
+
+  watch: {
+    history: function () {
+      this.$nextTick(scrollBottom);
+    },
+  },
+
+  components: {
+    MessageBubble,
+  },
+};
+</script>
