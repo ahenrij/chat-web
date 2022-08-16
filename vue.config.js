da6a3dc547f2d574ffaf977e0ec3e703b541e495
Henri Aïdasso <ahenrij@gmail.com>
2022-08-16 21:23:52 +0200

    ws config added

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,14 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      // proxy ws calls to sails.io
+      "^/socket.io": {
+        target: "http://localhost:1337/",
+        ws: true,
+        // changeOrigin: true
+      },
+    },
+  },
 });
